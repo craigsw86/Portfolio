3d87ccc1caa72dab955e070a9e6dccc4c9123799
Craig Weinstein <craig.s.weinstein@gmail.com>
2020-11-28 10:08:52 -0500

    New Project

---
+++ HomeTheater/HomeTheater.js
@@ -0,0 +1,41 @@
+class Movie {
+    constructor(title, year, genre, director, language) {
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.director = director;
+        this.language = language;
+    }
+}
+let watchedMovie1 = new Movie("Fail Safe", 1964, "Thriller", "Sidney Lumet", "English");
+let watchedMovie2 = new Movie("Dersu Uzala", 1975, "Historical", "Akira Kurosawa", "Russian");
+let watchedMovie3 = new Movie("Madadayo", 1993, "Drama", "Akira Kurosawa", "Japanese");
+let watchedMovie4 = new Movie("American History X", 1998, "Drama", "Tony Kaye", "English");
+let watchedMovie5 = new Movie("Accepted", 2006, "Comedy", "Steve Pink", "English");
+let watchedMovie6 = new Movie("Bonjour Monsieur Shlomi", 2003, "Comedy", "Shemi Zarchin", "Hebrew");
+let watchedMovie7 = new Movie("Aviva, My Love", 2006, "Drama", "Shemi Zarchin", "Hebrew");
+let watchedMovie8 = new Movie("The Wooden Gun", 1979, "Drama", "Ilan Moshenson", "Hebrew");
+let watchedMovie9 = new Movie("Campfire", 2004, "Drama", "Joseph Cedar", "Hebrew");
+let watchedMovie10 = new Movie("Off White Lies", 2011, "Drama", "Maya Kenig", "Hebrew");
+let watchedMovie11 = new Movie("Operation Grandma", 1999, "Comedy", "Dror Shaul", "Hebrew");
+let watchedMovie12 = new Movie("The War of the Roses", 1989, "Comedy", "Danny DeVito", "English");
+let watchedMovie13 = new Movie("1945", 2017, "Drama", "Ferenc Török", "Hungarian");
+let watchedMovie14 = new Movie("Z", 1969, "Drama", "Costa-Gavras", "French");
+let watchedMovie15 = new Movie("The Confession", 1970, "Drama", "Costa-Gavras", "French");
+let watchedMovie16 = new Movie("Parasite", 2019, "Comedy", "Bong Joon-ho", "Korean");
+let watchedMovie17 = new Movie("The Host", 2006, "Monster", "Bong Joon-ho", "Korean");
+let watchedMovie18 = new Movie("Save the Green Planet!", 2003, ["Science-Fiction", "Comedy"], "Jang Joon-hwan", "Korean");
+let watchedMovie19 = new Movie("The Bad Sleep Well", 1960, "Mystery", "Akira Kurosawa", "Japanese");
+let watchedMovie20 = new Movie("Stray Dog", 1949, "Drama", "Akira Kurosawa", "Japanese");
+let watchedMovie21 = new Movie("Drunken Angel", 1948, "Drama", "Akira Kurosawa", "Japanese");
+let watchedMovie22 = new Movie("Dodes'ka-den", 1970, "Drama", "Akira Kurosawa", "Japanese");
+let watchedMovie23 = new Movie("Red Beard", 1965, "Drama", "Akira Kurosawa", "Japanese");
+let watchedMovie24 = new Movie("Dora-heita", 2000, "Drama", "Kon Ichikawa", "Japanese");
+let watchedMovie25 = new Movie("White Material", 2009, "Drama", "Claire Denis", "French");
+let watchedMovie26 = new Movie("Danton", 1983, "Drama", "Andrzej Wajda", "French");
+let watchedMovie27 = new Movie("Kuroneko", 1968, "Horror", "Kaneto Shindo", "Japanese");
+let watchedMovie28 = new Movie("Onibaba", 1964, "Horror", "Kaneto Shindo", "Japanese");
+let watchedMovie29 = new Movie("Harakiri", 1962, "Drama", "Masaki Kobayashi", "Japanese");
+let watchedMovie30 = new Movie("Europa Europa", 1990, "Drama", "Agnieszka Holland", ["German", "Russian", "Polish", "Hebrew", "Yiddish"]);
+let watchedMovie31 = new Movie("Phoenix", 2014, "Drama", "Christian Petzold", "German");
+let watchedMovie32 = new Movie("Cold War", 2018, "Drama", "Paweł Pawlikowski", ["Polish", "French"]);
