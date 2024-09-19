1b6ff04cc3dc2d901af13709565031d9e272548b
Craig Weinstein <craig.s.weinstein@gmail.com>
2024-09-19 15:54:58 -0400

    some more updates

---
+++ Remind2Shred/Remind2Shred.py
@@ -0,0 +1,16 @@
+reminders = []
+
+from datetime import datetime
+import time
+time.strftime('%Y-%m-%d')
+
+reminders = [datetime.fromisoformat(reminder1).timestamp(): reminder1 for reminder in reminders]
+
+while True:
+    now = time.time()
+    for timestamp, reminder_msg in reminders2.items():
+        if timestamp < now:
+            print(reminder_msg)
+            del reminders2[timestamp]
+            break
+    time.sleep(1)
---
+++ Remind2Shred/Remind2Shred_2.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+connection = sqlite3.connect("gfg.db")
+
+crsr = connection.cursor()
+
+sql_command = """CREATE TABLE emp (
+patient_number INTEGER PRIMARY KEY,
+fname VARCHAR(30),
+lname VARCHAR(30),
+lastvisit DATE);"""
+
+pk = [2, 3, 4]
+
+fname = ['John', 'Joe', 'Anna']
+
+lname = ['Smith', 'Jones', 'Roberts']
+
+lastvisit = ['2020-01-01', '1991-12-31', '2024-01-01']
+
+for i in range(3):
+        crsr.execute('INSERT INTO emp VALUES ({pk[i]}, "{f_name[i]}", "{l_name[i]}", "{date[i]}")')
+
+connection.commit()
+
+connection.close()
