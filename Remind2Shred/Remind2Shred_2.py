e1ceb34ac39e72e34db5abd262decb9d5003eabc
Craig Weinstein <craig.s.weinstein@gmail.com>
2025-04-24 14:48:05 -0400

    UPDATE

--- Remind2Shred/Remind2Shred_2.py
+++ Remind2Shred/Remind2Shred_2.py
@@ -1,4 +1,9 @@
 import sqlite3
+import pandas
+import mysql.connector
+import schedule
+import time
+from datetime import datetime, timedelta
 
 connection = sqlite3.connect("gfg.db")
 
@@ -12,15 +17,21 @@ lastvisit DATE);"""
 
 pk = [2, 3, 4]
 
-fname = ['John', 'Joe', 'Anna']
+fname = ["John", "Joe", "Anna"]
 
-lname = ['Smith', 'Jones', 'Roberts']
+lname = ["Smith", "Jones", "Roberts"]
 
-lastvisit = ['2020-01-01', '1991-12-31', '2024-01-01']
+lastvisit = ["2020-01-01", "1991-12-31", "2024-01-01"]
 
 for i in range(3):
-        crsr.execute('INSERT INTO emp VALUES ({pk[i]}, "{f_name[i]}", "{l_name[i]}", "{date[i]}")')
+    crsr.execute(
+        'INSERT INTO emp VALUES ({pk[i]}, "{f_name[i]}", "{l_name[i]}", "{date[i]}")'
+    )
 
 connection.commit()
 
 connection.close()
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
